35c6141a256d43be59fcab29f0c7a6812a509c32
divyanshjain23 <divyansh.jain@joshtechnologygroup.com>
2025-06-15 19:58:32 +0530

    Added Responsivenes

--- src/script/index.js
+++ src/script/index.js
@@ -1,2 +1,28 @@
 import '../styles/main.scss';
 console.log('console log ');
+
+const hamburgerBtn = document.getElementById('hamburger-btn');
+let isMenuOpen = false;
+hamburgerBtn.addEventListener('click', () => {
+    const navLinks = document.querySelector('.navbar_nav-links');
+    const authLinks = document.querySelector('.navbar_auth-links');
+
+    if (window.innerWidth < 768) {
+        // mobile view
+        if (isMenuOpen) {
+            // menu is open
+            navLinks.style.display = 'none';
+            authLinks.style.display = 'none';
+        } else {
+            navLinks.style.display = 'block';
+            authLinks.style.display = 'block';
+        }
+    } else if (window.innerWidth < 1200) {
+        if (isMenuOpen) {
+            navLinks.style.display = 'none';
+        } else {
+            navLinks.style.display = 'block';
+        }
+    }
+    isMenuOpen = !isMenuOpen;
+});
