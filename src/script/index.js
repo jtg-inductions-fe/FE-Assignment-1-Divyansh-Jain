01a6a83491f1ce8838a43e62c928d3f7e01280ff
divyanshjain23 <divyansh.jain@joshtechnologygroup.com>
2025-06-17 16:50:55 +0530

    Update class names for html tags

--- src/script/index.js
+++ src/script/index.js
@@ -1,27 +1,27 @@
 import '../styles/main.scss';
 
-const menuBtn = document.querySelector('.navbar_hamburger-btn');
-const navLinks = document.querySelector('.navbar_nav-links');
-const authBtns = document.querySelector('.navbar_auth-btns');
-const menuCloseIcon = document.querySelector('.navbar_hamburger-btn--close');
-const menuOpenIcon = document.querySelector('.navbar_hamburger-btn--open');
+const menuBtn = document.querySelector('.navbar__hamburger-btn');
+const navLinks = document.querySelector('.navbar__nav-links');
+const authBtns = document.querySelector('.navbar__auth-btns');
+const menuCloseIcon = document.querySelector('.navbar__hamburger-btn--close');
+const menuOpenIcon = document.querySelector('.navbar__hamburger-btn--open');
 let isMenuOpen = false;
 
 menuBtn.addEventListener('click', () => {
     if (isMenuOpen) {
         // menu is in open state and is closed by button click
-        navLinks.className = 'navbar_nav-links--open';
-        authBtns.className = 'navbar_auth-btns--open';
-        menuCloseIcon.className = 'navbar_hamburger-btn--close';
-        menuOpenIcon.className = 'navbar_hamburger-btn--open hidden';
+        navLinks.className = 'navbar__nav-links--open';
+        authBtns.className = 'navbar__auth-btns--open';
+        menuCloseIcon.className = 'navbar__hamburger-btn--close';
+        menuOpenIcon.className = 'navbar__hamburger-btn--open hidden';
         // document.body.style.overflow = "hidden " is used to disable scroll when the menu is in open state
         document.body.style.overflow = 'hidden';
     } else {
         // menu is in closed state
-        navLinks.className = 'navbar_nav-links';
-        authBtns.className = 'navbar_auth-btns';
-        menuCloseIcon.className = 'navbar_hamburger-btn--close hidden';
-        menuOpenIcon.className = 'navbar_hamburger-btn--open';
+        navLinks.className = 'navbar__nav-links';
+        authBtns.className = 'navbar__auth-btns';
+        menuCloseIcon.className = 'navbar__hamburger-btn--close hidden';
+        menuOpenIcon.className = 'navbar__hamburger-btn--open';
         document.body.style.overflow = 'auto';
     }
     isMenuOpen = !isMenuOpen;
