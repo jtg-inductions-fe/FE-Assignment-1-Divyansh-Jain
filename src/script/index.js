cb242cbdfb9e0b0fa013547cf05a040002a26c0a
divyanshjain23 <divyansh.jain@joshtechnologygroup.com>
2025-06-17 16:02:57 +0530

    Added comments to  files

--- src/script/index.js
+++ src/script/index.js
@@ -9,16 +9,19 @@ let isMenuOpen = false;
 
 menuBtn.addEventListener('click', () => {
     if (isMenuOpen) {
+        // menu is in open state and is closed by button click
         navLinks.className = 'navbar_nav-links--open';
         authBtns.className = 'navbar_auth-btns--open';
-        menuCloseIcon.className = 'navbar-hamburger-btn--close';
-        menuOpenIcon.className = 'navbar-hamburger-btn--open hidden';
+        menuCloseIcon.className = 'navbar_hamburger-btn--close';
+        menuOpenIcon.className = 'navbar_hamburger-btn--open hidden';
+        // document.body.style.overflow = "hidden " is used to disable scroll when the menu is in open state
         document.body.style.overflow = 'hidden';
     } else {
+        // menu is in closed state
         navLinks.className = 'navbar_nav-links';
         authBtns.className = 'navbar_auth-btns';
-        menuCloseIcon.className = 'navbar-hamburger-btn--close hidden';
-        menuOpenIcon.className = 'navbar-hamburger-btn--open';
+        menuCloseIcon.className = 'navbar_hamburger-btn--close hidden';
+        menuOpenIcon.className = 'navbar_hamburger-btn--open';
         document.body.style.overflow = 'auto';
     }
     isMenuOpen = !isMenuOpen;
