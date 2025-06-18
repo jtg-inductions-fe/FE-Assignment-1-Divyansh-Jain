036248faa70e0feeb9956dad21112d2bc2ed3f27
divyanshjain23 <divyansh.jain@joshtechnologygroup.com>
2025-06-18 16:11:58 +0530

    Added mixins for flex and font property

--- src/script/index.js
+++ src/script/index.js
@@ -1,28 +1,17 @@
 import '../styles/main.scss';
 
-const menuBtn = document.querySelector('.navbar__hamburger-btn');
-const navLinks = document.querySelector('.navbar__nav-links');
-const authBtns = document.querySelector('.navbar__auth-btns');
-const menuCloseIcon = document.querySelector('.navbar__hamburger-btn--close');
-const menuOpenIcon = document.querySelector('.navbar__hamburger-btn--open');
-let isMenuOpen = false;
-
+const menuBtn = document.getElementById('js-menu-btn');
+const navbar = document.getElementById('js-navbar');
+const menuIcon = document.getElementById('js-menu-icon');
+const imagePath = 'src/assets/images';
+const openIcon = 'hamburger-btn.png';
+const closeIcon = 'menu-close.png';
 menuBtn.addEventListener('click', () => {
-    if (isMenuOpen) {
-        // menu is in open state and is closed by button click
-        navLinks.className = 'navbar__nav-links--open';
-        authBtns.className = 'navbar__auth-btns--open';
-        menuCloseIcon.className = 'navbar__hamburger-btn--close';
-        menuOpenIcon.className = 'navbar__hamburger-btn--open hidden';
-        // document.body.style.overflow = "hidden " is used to disable scroll when the menu is in open state
-        document.body.style.overflow = 'hidden';
+    if (!navbar.classList.toggle('navbar--active')) {
+        navbar.className = 'navbar';
+        menuIcon.src = `${imagePath}/${closeIcon}`;
     } else {
-        // menu is in closed state
-        navLinks.className = 'navbar__nav-links';
-        authBtns.className = 'navbar__auth-btns';
-        menuCloseIcon.className = 'navbar__hamburger-btn--close hidden';
-        menuOpenIcon.className = 'navbar__hamburger-btn--open';
-        document.body.style.overflow = 'auto';
+        menuIcon.src = `${imagePath}/${openIcon}`;
+        navbar.className = 'navbar navbar--active';
     }
-    isMenuOpen = !isMenuOpen;
 });
