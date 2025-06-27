b5f1219521a29fa68ec3ffba4b6f4d97a33cd73d
divyanshjain23 <divyansh.jain@joshtechnologygroup.com>
2025-06-27 20:04:19 +0530

    bug fixes

--- src/script/carousel.js
+++ src/script/carousel.js
@@ -2,10 +2,11 @@ import Splide from '@splidejs/splide';
 var splide = new Splide('.splide', {
     classes: {
         arrows: 'splide__arrows testimonial__btns',
-        prev: 'splide__arrow--prev your-class-prev testimonial__btn--next',
+        prev: 'splide__arrow--prev testimonial__btn--next',
         arrow: 'splide__arrow  testimonial__btn',
-        next: 'splide__arrow--next your-class-next testmonial__btn--prev',
+        next: 'splide__arrow--next  testmonial__btn--prev',
     },
+    type: 'loop',
 });
 
 export default function mountCarousel() {
